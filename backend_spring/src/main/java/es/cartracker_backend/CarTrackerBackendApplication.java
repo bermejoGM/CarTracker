7bd2fedfcc93f6e82e7f5bdea89ddc16c1064d19
Diego <diegobermejo045@gmail.com>
2026-06-09 15:43:46 +0200

    first commit

---
+++ backend_spring/src/main/java/es/cartracker_backend/CarTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package es.cartracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarTrackerBackendApplication.class, args);
+    }
+
+}
---
+++ backend_spring/src/test/java/es/cartracker_backend/CarTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cartracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
